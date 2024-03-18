f7353ec7f26f3707e02894f1eae341812ec95ea6
Vermon <vermon.aguiar@edu.pucrs.br>
2024-03-18 16:04:52 -0300

    input validation start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const cors = require('cors')
 
 //
-mongoose.connect('mongodb+srv://vermoncom:KL8KZy1sU4YeTB7x@mega-sena-dell-api.4wgsdit.mongodb.net/?retryWrites=true&w=majority&appName=mega-sena-dell-api')
+mongoose.connect('mongodb+srv://vermoncom:KL8KZy1sU4YeTB7x@mega-sena-dell-api.4wgsdit.mongodb.net/?retryWrites=true&w=majority&appName=mega-sena-dell-api').then(
+    ()=> console.log("CONNECTED")
+)
 app.use(express.json())
+app.use(cors())
+
+
 const port = 3000
 
 
@@ -22,11 +28,20 @@ app.get('/apostas', async (req, res) => {
 })
 
 app.delete('/deleta/:id', async (req,res) => {
-    const aposta = await Aposta.findOneAndDelete({"id": req.params.id})
+    const aposta = await Aposta.findOneAndDelete({"_id": req.params.id})
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Request-Width, Content-Type, Accept");    
     res.send(aposta)
 })
 
 app.post('/cadastra', async (req, res) => {
+    if(req.body.id === undefined && req.body.name === undefined && req.body.cpf === undefined && req.body.numeros === undefined && req.body.id === undefined
+        && req.body.id === "" && req.body.name === "" && req.body.cpf === "" || req.body.numeros.length === 0){
+        console.log(req.body.id, req.body.name, req.body.cpf, req.body.numeros)
+        console.log(req.body)
+        res.send("Tente novamente após preencher corretamente os campos.")
+        return
+    }
     const aposta = new Aposta({
         id: req.body.id,
         name: req.body.name,
