6122fed0bb6016390496ba08ca1e3eb68682a5e0
Vermon <vermon.aguiar@edu.pucrs.br>
2024-03-19 15:53:57 -0300

    sorteio pronto, faltando numeros mais apostados

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const port = 3000
 
 var idGlobal = 1000;
 
+var qnt_numeros_apostados = Array(50).fill(0)
 
 const Aposta = mongoose.model('Aposta', {
     id: Number,
@@ -23,21 +24,110 @@ const Aposta = mongoose.model('Aposta', {
     numeros: [Number]
 });
 
+const getRandomInt = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+app.get('/sorteio', async (req,res) => {
+
+    var apostas_vencedoras = []
+    var numeros_sorteados = []
+    var rodada = 0
+    var vencedor = false
+
+    const apostas = await Aposta.find().then(apostas => {
+
+    for(var i = 0 ; i < 5; i++ ){
+        var m = getRandomInt(1, 51)
+        if(numeros_sorteados.includes(m)){
+            i--
+            continue
+        }
+        numeros_sorteados.push(m)
+        console.log(m)
+    }
+        
+    while(rodada < 25){
+        apostas.forEach(element => {
+            //para todos os numeros da aposta
+            var numeros_acertados = 0
+            element.numeros.forEach(n => {
+                //verifica se esse numero esta entre os sorteados
+                if(numeros_sorteados.includes(n)){
+                    numeros_acertados++                
+                }
+                //se chegar a 5, ou seja, acertou todos, entra para o array de apostas vencedoras
+                if(numeros_acertados === 5){
+                    apostas_vencedoras.push(element)
+                    vencedor = true
+                }
+            })
+    
+        })
+        if(vencedor === true){
+            break
+        }
+        rodada++
+        var l = getRandomInt(1, 51)
+        while(numeros_sorteados.includes(l)){
+            l = getRandomInt(1, 51)
+        }
+        numeros_sorteados.push(l)
+    }
+    }).finally(() => {
+        const resposta = {
+            "venceu": vencedor,
+            "numeros_sorteados": numeros_sorteados,
+            "rodada": rodada,
+            "apostas_vencedoras": apostas_vencedoras,
+            "numeros_apostados": qnt_numeros_apostados
+        }
+        res.send(resposta)
+        console.log(resposta)
+        //console.log(resposta)
+    })
+    
+})
+
+// app.get('/numerosApostados', async (req, res) => {
+//     res.send(qnt_numeros_apostados)
+// })
 
 app.get('/apostas', async (req, res) => {
-    const apostas = await Aposta.find()
-    res.send(apostas)
+    const apostas = await Aposta.find().then(apostas => {
+        qnt_numeros_apostados = Array(50).fill(0)
+        apostas.forEach(aposta => {
+
+            aposta.numeros.forEach(element => {
+                qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
+            });
+
+        });
+        
+
+        res.send(apostas)
+    })
 })
 
 app.delete('/deleta/:id', async (req,res) => {
+    //apenas para debug, deleta toda a colecao.
     if(req.params.id == 666){
         await Aposta.deleteMany({})
     } 
+
+    req.body.numeros.forEach(element => {
+        qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
+    });
+
     const aposta = await Aposta.findOneAndDelete({"_id": req.params.id})
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Request-Width, Content-Type, Accept");    
+ 
     res.send(aposta)
 })
+
+
+
 
 app.post('/cadastra', async (req, res) => {
     if(req.body.name === undefined || req.body.cpf === undefined || req.body.numeros === undefined
@@ -51,6 +141,12 @@ app.post('/cadastra', async (req, res) => {
 
     const lastId = await Aposta.countDocuments() + 1
     console.log(lastId)
+
+    req.body.numeros.forEach(element => {
+        qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
+        
+    });
+    console.log(qnt_numeros_apostados)
 
     const aposta = new Aposta({
         id: lastId + 1000,
