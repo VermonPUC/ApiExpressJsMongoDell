6e8409cc02644617024169733cc793ea2a1d5ea7
Vermon <vermon.aguiar@edu.pucrs.br>
2024-03-20 12:58:06 -0300

    contagem de numeros apostados

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ app.use(cors())
 
 const port = 3000
 
-var idGlobal = 1000;
-
-var qnt_numeros_apostados = Array(50).fill(0)
-
 const Aposta = mongoose.model('Aposta', {
     id: Number,
     name: String,
@@ -33,50 +29,58 @@ const getRandomInt = (min, max) => {
 app.get('/sorteio', async (req,res) => {
 
     var apostas_vencedoras = []
-    var numeros_sorteados = []
+    var numeros_sorteados = [15,17,10,11,48]
     var rodada = 0
     var vencedor = false
 
-    const apostas = await Aposta.find().then(apostas => {
+    await Aposta.find().sort({name:1}).then(apostas => {
 
-    for(var i = 0 ; i < 5; i++ ){
-        var m = getRandomInt(1, 51)
-        if(numeros_sorteados.includes(m)){
-            i--
-            continue
+        qnt_numeros_apostados = Array(50).fill(0)
+        apostas.forEach(aposta => {
+            aposta.numeros.forEach(element => {
+                qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element -1] + 1
+            });
+
+        });
+
+        for(var i = 0 ; i < 5; i++ ){
+            var m = getRandomInt(1, 51)
+            if(numeros_sorteados.includes(m)){
+                i--
+                continue
+            }
+            numeros_sorteados.push(m)
+            console.log(m)
         }
-        numeros_sorteados.push(m)
-        console.log(m)
-    }
+            
+        while(rodada < 25){
+            apostas.forEach(element => {
+                //para todos os numeros da aposta
+                var numeros_acertados = 0
+                element.numeros.forEach(n => {
+                    //verifica se esse numero esta entre os sorteados
+                    if(numeros_sorteados.includes(n)){
+                        numeros_acertados++                
+                    }
+                    //se chegar a 5, ou seja, acertou todos, entra para o array de apostas vencedoras
+                    if(numeros_acertados === 5){
+                        apostas_vencedoras.push(element)
+                        vencedor = true
+                    }
+                })
         
-    while(rodada < 25){
-        apostas.forEach(element => {
-            //para todos os numeros da aposta
-            var numeros_acertados = 0
-            element.numeros.forEach(n => {
-                //verifica se esse numero esta entre os sorteados
-                if(numeros_sorteados.includes(n)){
-                    numeros_acertados++                
-                }
-                //se chegar a 5, ou seja, acertou todos, entra para o array de apostas vencedoras
-                if(numeros_acertados === 5){
-                    apostas_vencedoras.push(element)
-                    vencedor = true
-                }
             })
-    
-        })
-        if(vencedor === true){
-            break
+            if(vencedor === true){
+                break
+            }
+            rodada++
+            var l = getRandomInt(1, 51)
+            while(numeros_sorteados.includes(l)){
+                l = getRandomInt(1, 51)
+            }
+            numeros_sorteados.push(l)
         }
-        rodada++
-        var l = getRandomInt(1, 51)
-        while(numeros_sorteados.includes(l)){
-            l = getRandomInt(1, 51)
-        }
-        numeros_sorteados.push(l)
-    }
-    }).finally(() => {
+
         const resposta = {
             "venceu": vencedor,
             "numeros_sorteados": numeros_sorteados,
@@ -86,9 +90,7 @@ app.get('/sorteio', async (req,res) => {
         }
         res.send(resposta)
         console.log(resposta)
-        //console.log(resposta)
-    })
-    
+        })
 })
 
 // app.get('/numerosApostados', async (req, res) => {
@@ -96,17 +98,7 @@ app.get('/sorteio', async (req,res) => {
 // })
 
 app.get('/apostas', async (req, res) => {
-    const apostas = await Aposta.find().then(apostas => {
-        qnt_numeros_apostados = Array(50).fill(0)
-        apostas.forEach(aposta => {
-
-            aposta.numeros.forEach(element => {
-                qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
-            });
-
-        });
-        
-
+    await Aposta.find().then(apostas => {
         res.send(apostas)
     })
 })
@@ -115,14 +107,10 @@ app.delete('/deleta/:id', async (req,res) => {
     //apenas para debug, deleta toda a colecao.
     if(req.params.id == 666){
         await Aposta.deleteMany({})
-    } 
-
-    req.body.numeros.forEach(element => {
-        qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
-    });
-
-    const aposta = await Aposta.findOneAndDelete({"_id": req.params.id})
- 
+        res.send("Apostas deletadas")
+        return
+    }
+    const aposta = await Aposta.findOneAndDelete({"id": req.params.id})
     res.send(aposta)
 })
 
@@ -142,14 +130,8 @@ app.post('/cadastra', async (req, res) => {
     const lastId = await Aposta.countDocuments() + 1
     console.log(lastId)
 
-    req.body.numeros.forEach(element => {
-        qnt_numeros_apostados[element -1] = qnt_numeros_apostados[element] + 1
-        
-    });
-    console.log(qnt_numeros_apostados)
-
     const aposta = new Aposta({
-        id: lastId + 1000,
+        id: lastId + 999,
         name: req.body.name,
         cpf: req.body.cpf,
         numeros: req.body.numeros
